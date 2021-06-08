22d2c282e64de1409beb4a5831acf4bab1f4e15b
Lebibi <lebihannathan@gmail.com>
2021-06-08 16:13:03 +0200

    tu coco

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nle-biha <nle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 16:10:56 by nle-biha          #+#    #+#             */
-/*   Updated: 2021/06/08 15:32:16 by nle-biha         ###   ########.fr       */
+/*   Updated: 2021/06/08 16:12:29 by nle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,16 @@ void	runcmd1(int filfd, int *pipfd, char **cmd, char **paths)
 	return ;
 }
 
+void	close_all(int *pipfd, int *filfd, char **cmd, char **paths)
+{
+	close(pipfd[1]);
+	close(pipfd[0]);
+	close(filfd[0]);
+	close(filfd[1]);
+	free_nulltermchartab(cmd);
+	free_nulltermchartab(paths);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char	**paths;
@@ -60,21 +70,24 @@ int	main(int argc, char **argv, char **envp)
 	char	**cmd;
 
 	if (argc != 5)
-		return (0);
-	pipe(pipfd);
+		return (1);
 	paths = ft_split(get_env_value("PATH", envp), ':');
-	filfd[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 644);
+	filfd[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	filfd[0] = open(argv[1], O_RDONLY);
+	if (filfd[0] == -1 || filfd[1] == -1 || paths == NULL || pipe(pipfd) == -1)
+	{
+		perror("Error ");
+		return (1);
+	}
 	cmd = ft_split(argv[2], ' ');
-	runcmd1(filfd[0], pipfd, cmd, paths);
-	free_nulltermchartab(cmd);
-	cmd = ft_split(argv[3], ' ');
-	runcmd2(filfd[1], pipfd, cmd, paths);
-	close(pipfd[1]);
-	close(pipfd[0]);
-	close(filfd[0]);
-	close(filfd[1]);
-	free_nulltermchartab(cmd);
-	free_nulltermchartab(paths);
+	if (cmd != NULL)
+	{
+		runcmd1(filfd[0], pipfd, cmd, paths);
+		free_nulltermchartab(cmd);
+		cmd = ft_split(argv[3], ' ');
+		if (cmd != NULL)
+			runcmd2(filfd[1], pipfd, cmd, paths);
+	}
+	close_all(filfd, pipfd, paths, cmd);
 	return (0);
 }
